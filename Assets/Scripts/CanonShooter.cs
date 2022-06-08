5f44fb2a76ffaff15c514b739d8fedb87ad2744e
tuancq <tuancq2011@gmail.com>
2022-06-08 20:01:43 +0700

    Add Power Bar Function

--- Assets/Scripts/CanonShooter.cs
+++ Assets/Scripts/CanonShooter.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CanonShooter : MonoBehaviour
 {
     [SerializeField] private GameObject canonBall;
     [SerializeField] private Transform shotPoint;
-    public float shotPower;
+    [SerializeField] private Slider powerBar;
+
+    public float sensitivity;
+    private float shotPower;
     public int ballLeft;
 
     private void Start()
@@ -16,15 +20,30 @@ public class CanonShooter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ChargePowerBar();
+        ReleaseCharge();
+    }
+
+    void ChargePowerBar()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            powerBar.value += Time.deltaTime * sensitivity;
+        }
+    }
+
+    void ReleaseCharge()
+    {
         if (ballLeft > 0)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyUp(KeyCode.Space))
             {
                 GameObject newCanonBall = Instantiate(canonBall, shotPoint.position, shotPoint.rotation);
                 ballLeft--;
-                newCanonBall.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, shotPower), ForceMode.Impulse);
+                newCanonBall.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, powerBar.value), ForceMode.Impulse);
+                powerBar.value = 0; 
             }
-        } 
+        }
     }
 
     public int GetBallLeft()
